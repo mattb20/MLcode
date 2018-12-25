b53cc76e62387e5a53038ba6c3295f010776012d
mattb20 <matt_burgoyne@live.co.uk>
2018-12-25 08:07:07 +0000

    stashed commit

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.cross_validation import train_test_split
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
 
-imputer = Imputer(missing_values) = "NaN", strategy = "mean", axis = 0)
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
@@ -20,3 +20,7 @@ labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
