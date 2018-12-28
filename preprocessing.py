c94ee77d598ec2cd76a468ffcc5a38915a9abc2f
mattb20 <matt_burgoyne@live.co.uk>
2018-12-28 08:16:34 +0000

    fix errors with imports

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
-from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
 
-imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+imputer = SimpleImputer(missing_values = np.NaN, strategy = "mean")
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
